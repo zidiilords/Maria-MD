905e292e7b2a3e7d2513e000a2f1c15e456e5789
Ayush <145197795+AYUSH-PANDEY023@users.noreply.github.com>
2024-04-18 06:40:05 +0530

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,70 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+
+//contact details
+global.ownernumber = ['919931122319']
+global.ownernumberr = ['919931122319']
+global.ownername = "OLDUSER"//owner name
+global.ytname = "YT: AyushBotz.inc"
+global.socialm = "GitHub: Ayushpandey023"
+global.location = "Indian"
+
+global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+
+//sticker details
+global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
+global.packname = 'Sticker By'
+global.author = 'Maria Bot'
+//console view/theme
+global.themeemoji = '🧩'
+global.wm = "Ayush botz inc."
+
+//theme link
+global.link = 'https://whatsapp.com/channel/0029VaImo5ZG3R3qjKOdyr1I'
+
+//custom prefix
+global.prefa = ['#']
+
+//false=disable and true=enable
+global.welcome = false //auto welcome
+global.autoRecording = false //auto recording
+global.autoTyping = false //auto typing
+global.autorecordtype = false //auto typing + recording
+global.autoread = false //auto read messages
+global.autobio = false //auto update bio
+global.anti212 = true //auto block +212
+global.autoread_status = false //auto view status/story
+
+
+
+//reply messages
+global.mess = {
+    done: '🍭 *Here you go, darling!* ',
+    prem: '🍭 *My darling, this feature is reserved for premium users only*',
+    admin: '🍭 *My darling, this feature is reserved for admins only*',
+    botAdmin: '🍭 *Darling, this feature can only be used when the bot is a group admin* ',
+    owner: '🍭 *My dearest, this feature is reserved for the owner only*',
+    group: '🍭 *Sweetheart, this feature is exclusively for groups*',
+    private: '🍭 *My love, this feature is exclusively for private chats*',
+    wait: '🍭 *Darling, in process...* ',    
+    error: '🍭 *Oh darling, there seems to be an error!*',
+}
+
+module.exports = {
+sessionId: process.env.id || "_M_A_R_I_A_VEdGdFltUmhSVzFpY21GalpRbz0=",
+AUTO_BLOCK : "true",
+STATUS_SAVER : "true",
+ANTI_BOT:"true",
+}
+
+
+global.thumb = fs.readFileSync('./Gallery/thumb.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+    fs.unwatchFile(file)
+    console.log(chalk.redBright(`Update'${__filename}'`))
+    delete require.cache[file]
+    require(file)
+})
