fef40eeac8d1c5657bf7f2deae833e882cbb0f90
Teamolduser <120130487+Teamolduser@users.noreply.github.com>
2024-04-23 20:32:58 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 //contact details
 global.ownernumber = process.env.OWNER_NUMBER || "919931122319",
-global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",//owner name
+global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
@@ -51,6 +51,8 @@ global.mess = {
 }
 
 module.exports = {
+ownernumber: global.ownernumber,
+ownername: global.ownername,
 sessionId: process.env.id,
 AUTO_BLOCK : process.env.PM_BLOCKER,
 STATUS_SAVER : "true",
